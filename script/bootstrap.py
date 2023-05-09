ab212a20ca52cf09e1cd31b7ce47fa281912486b
Armand Sauzay <35524799+armand-sauzay@users.noreply.github.com>
2023-05-09 20:52:19 +0000

    fix(bootstrap): update file copy strategy

--- script/bootstrap.py
+++ script/bootstrap.py
@@ -6,8 +6,10 @@ This script is used to bootstrap the project. It will:
 """
 
 import os
+import shutil
 import subprocess
 import sys
+import time
 from pathlib import Path
 
 COLORS = {
@@ -19,39 +21,47 @@ COLORS = {
     "cyan": "36",
 }
 
+
 LINE_LENGTH = 80
 
 
-def color_print(text, color):
+def color_print(text, color, end="\n"):
     """Print text in color"""
-    print(f"\033[{COLORS[color]}m{text}\033[0m")
+    print(f"\033[{COLORS[color]}m{text}\033[0m", end=end)
 
 
+def print_progress(func):
+    """Print progress"""
+
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__} with args {args} and kwargs {kwargs}")
+        color_print(f"🔄 {func.__doc__}", "yellow", end="\r")
+        time.sleep(1)
+        result = func(*args, **kwargs)
+        print("\033[K", end="")
+        color_print(f"✅ {func.__doc__}", "green")
+        return result
+
+    return wrapper
+
+
+@print_progress
 def clean_repository():
-    """Clean the repository"""
+    """Removing python_template and tests folder"""
     subprocess.run(
         ["rm", "-rf", "tests", "python_template", "python-template"],
         stdout=subprocess.DEVNULL,
     )
 
 
+@print_progress
 def check_install(tool, installation_method):
-    """Check if tool is installed"""
+    """Checking if tool is installed with installation_method"""
     if installation_method not in ["pip", "brew"]:
         raise ValueError("Installation method not supported")
-    color_print(f"  {tool}: checking installation  ".center(LINE_LENGTH, "="), "yellow")
     try:
         subprocess.check_output([tool, "--version"])
-        color_print(
-            f"\u2705 {tool} is installed \u2705".center(LINE_LENGTH, " "), "green"
-        )
     except OSError:
-        color_print(
-            f"\u26A0 {tool} is not installed. Installing {tool} ... \u26A0".center(
-                LINE_LENGTH, " "
-            ),
-            "yellow",
-        )
         if installation_method == "pip":
             subprocess.check_call(
                 [sys.executable, "-m", "pip", "install", tool],
@@ -59,14 +69,11 @@ def check_install(tool, installation_method):
             )
         elif installation_method == "brew":
             subprocess.check_call(["brew", "install", tool], stdout=subprocess.DEVNULL)
-        # add white checkmark
-        color_print(
-            f"\u2705 {tool} has been installed \u2705".center(LINE_LENGTH, " "), "green"
-        )
-    print()
 
 
+@print_progress
 def get_git_root_folder_name():
+    """Getting the name of the git root folder"""
     try:
         git_root = (
             subprocess.check_output(["git", "rev-parse", "--show-toplevel"])
@@ -76,49 +83,31 @@ def get_git_root_folder_name():
         return Path(git_root).name
 
     except subprocess.CalledProcessError:
-        color_print(
-            "Not in a git repository. Exiting ...".center(LINE_LENGTH, " "),
-            "red",
-        )
+        color_print("Not in a git repository", "red")
         sys.exit(1)
 
 
+@print_progress
 def create_new_poetry_project():
-    """Initialize a poetry project"""
-    color_print("  Creating a new poetry project  ".center(LINE_LENGTH, "="), "yellow")
+    """Initialize a poetry project and add pytest"""
     git_root_folder_name = get_git_root_folder_name()
 
     if git_root_folder_name:
         subprocess.check_call(
             ["poetry", "new", git_root_folder_name], stdout=subprocess.DEVNULL
         )
-        color_print(
-            "\u2705 Poetry project created \u2705".center(LINE_LENGTH, " "), "green"
-        )
 
-    color_print(
-        "Copying files from template folder that do not exist".center(LINE_LENGTH, " "),
-        "yellow",
-    )
-    for file_name in os.listdir(git_root_folder_name):
-        source = os.path.join(git_root_folder_name, file_name)
-        destination = os.path.join(".", file_name)
-        if not os.path.exists(destination):
-            subprocess.check_call(
-                ["cp", "-r", source, destination], stdout=subprocess.DEVNULL
-            )
-            color_print(f"  {file_name} copied  ".center(LINE_LENGTH, " "), "green")
-    color_print("\u2705 Files copied \u2705".center(LINE_LENGTH, " "), "green")
+    temp_folder_name = "temp"
+    shutil.copytree(git_root_folder_name, temp_folder_name, dirs_exist_ok=True)
+    shutil.rmtree(git_root_folder_name)
+    files = os.listdir(temp_folder_name)
+    for file_name in files:
+        if os.path.isdir(os.path.join(temp_folder_name, file_name)):
+            shutil.copytree(os.path.join(temp_folder_name, file_name), file_name)
+        else:
+            shutil.copy2(os.path.join(temp_folder_name, file_name), file_name)
+    shutil.rmtree(temp_folder_name)
 
-    color_print("Removing the template folder".center(LINE_LENGTH, " "), "yellow")
-    subprocess.check_call(
-        ["rm", "-rf", git_root_folder_name], stdout=subprocess.DEVNULL
-    )
-    color_print(
-        "\u2705 Template folder removed \u2705".center(LINE_LENGTH, " "), "green"
-    )
-
-    color_print("Adding pytest to test dependencies".center(LINE_LENGTH, " "), "yellow")
     subprocess.check_call(
         ["poetry", "add", "--group", "test", "pytest"], stdout=subprocess.DEVNULL
     )
